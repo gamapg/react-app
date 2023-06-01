aed497f2a61bcdcd30d905b33c8f17bf08d7ebd3
gamapg <gamapg@yahoo.com>
2023-06-02 01:27:53 +0800

    tinkered with button, read react docs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from "./button.js";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MyButton />
       </header>
     </div>
   );
